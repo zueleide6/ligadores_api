b83402c9270f622841610488def482218648cc87
zueleide6 <161615388+zueleide6@users.noreply.github.com>
2024-03-05 00:59:30 -0300

    Update routes.jsasdasd

--- src/routes.js
+++ src/routes.js
@@ -7,8 +7,6 @@ const routes = express.Router();
 routes.get("/empresa", EmpresaController.listar);
 routes.post("/empresa", EmpresaController.criar);
 routes.get("/empresa/:empresa_id", EmpresaController.visualizar);
-routes.get('/empresa/bancos', EmpresaController.listarBancos);
-
 
 routes.post("/empresa/:empresa_id/atendimento", AtendimentoController.criar);
 routes.get("/empresa/:empresa_id/atendimento", AtendimentoController.listar);
