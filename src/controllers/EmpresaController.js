252178bde451c8da19add46d3111b27b86cf129b
zueleide6 <161615388+zueleide6@users.noreply.github.com>
2024-03-06 10:24:33 -0300

    Update EmpresaController.jsý

--- src/controllers/EmpresaController.js
+++ src/controllers/EmpresaController.js
@@ -29,35 +29,16 @@ module.exports = {
     const { cnpj } = req.params;
     
     const empresa = await Empresa.findOne({ cnpj })
-      .populate({
-        path: 'atendimentos',
-        options: { sort: { 'data': -1 }, limit: 1 } // Apenas o mais recente atendimento
-      })
-      .lean();
-
+    =
     if (!empresa) {
       return res.status(404).json({ error: "Empresa não encontrada" });
     }
-
-    // O atendimento mais recente estará disponível em `empresa.atendimentos[0]`, se existir
-    const status = empresa.atendimentos?.length > 0 ? empresa.atendimentos[0].status : 'Não Disponível';
-    const score = empresa.atendimentos?.length > 0 ? empresa.atendimentos[0].score :'Não Disponível';
-
-    // Atualize o objeto empresa com os novos campos para status e score
-    empresa.status = status;
-    empresa.score = score;
-
+    
     return res.json(empresa);
   },
   async listar(req, res) {
 
-    const empresas = await     Empresa.find().populate({
-      path: 'atendimentos', // Assumindo que você tem uma referência aos atendimentos em seu modelo de Empresa
-      options: { sort: { 'data': -1 } }, // Isso garante que o atendimento mais recente seja primeiro
-      limit: 1 // Limita para apenas o atendimento mais recente
-    })
-
-
+    const empresas = await     Empresa.find()
 
     return res.json(empresas);
   }
