80ad7ccf749e2237c173a5380efd3712250e5682
zueleide6 <161615388+zueleide6@users.noreply.github.com>
2024-03-06 00:05:46 -0300

    Update server.jsasd

--- src/server.js
+++ src/server.js
@@ -4,7 +4,12 @@ const routes = require("./routes");
 const app = express();
 const cors = require("cors");
 
-app.use(cors());
+const corsOptions = {
+  origin: 'https://starfish-app-s27ho.ondigitalocean.app', // ou '*' para permitir todos os domínios
+  allowedHeaders: ['Content-Type', 'Authorization'] // Adicione aqui todos os cabeçalhos que suas requisições utilizam
+};
+
+app.use(cors(corsOptions));
 
 mongoose.connect(
     "mongodb+srv://dopamina:Dopamina123@cluster0.2v3bl7g.mongodb.net/port"
