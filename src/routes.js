e6d388803332e1f1df0e8c5e703bc5aca745de2e
zueleide6 <161615388+zueleide6@users.noreply.github.com>
2024-03-06 09:54:57 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ routes.get("/empresa", EmpresaController.listar);
 routes.post("/empresa", EmpresaController.criar);
 routes.get("/empresa/:cnpj", EmpresaController.visualizar);
 
-routes.post("/empresa/:cnpj/atendimento", AtendimentoController.criar);
+routes.post("/empresa/atendimento/criar", AtendimentoController.criar);
 routes.get("/empresa/:cnpj/atendimento", AtendimentoController.listar);
 
 module.exports = routes;
